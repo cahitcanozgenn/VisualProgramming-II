a7e39a115157197ba4b3facd1a9557a69f6b8a31
Cahit Can ÖZGEN <75495961+cahitcanozgenn@users.noreply.github.com>
2023-04-03 14:25:29 -0700

    VisualProgramming-II

--- FourWeek/FourWeek/Form1.cs
+++ FourWeek/FourWeek/Form1.cs
@@ -101,7 +101,6 @@ namespace FourWeek
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
-
         }
 
         private void dataGridView1_SelectionChanged(object sender, EventArgs e)
@@ -114,16 +113,11 @@ namespace FourWeek
 
         private void button2_Click(object sender, EventArgs e)
         {
-          
                 i= toplamKayit-2;
                 textBox1.Text = dataGridView1.Rows[i].Cells[0].Value.ToString();
                 textBox2.Text = dataGridView1.Rows[i].Cells[1].Value.ToString();
                 textBox3.Text = dataGridView1.Rows[i].Cells[2].Value.ToString();
                 textBox4.Text = dataGridView1.Rows[i].Cells[3].Value.ToString();
-
-           
-          
-           
         }
     }
 }
